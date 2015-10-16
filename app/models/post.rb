6fed3bf620b92cf4fe0b48385968368454852af1
Lisa Folliard <lisaf37@gmail.com>
2015-10-16 11:52:27 -0700

    validate presence of post contributor

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ActiveRecord::Base
   belongs_to :Category
 
   validates :title, :presence => true
+  validates :contributor, :presence => true
+
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,4 +2,6 @@ require 'rails_helper'
 
 describe Post do
   it { should validate_presence_of :title }
+  it { should validate_presence_of :contributor }
+
 end
