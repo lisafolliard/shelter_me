61a6b47ebb7f58f10b8dae023e09a4f039ffa07a
Lisa Folliard <lisaf37@gmail.com>
2015-10-16 15:43:01 -0700

    pass integration spec to edit a category post

--- spec/features/add_post_pages_spec.rb
+++ spec/features/add_post_pages_spec.rb
@@ -16,3 +16,18 @@ describe "the add a post process" do
     expect(page).to have_content "Big Bike"
   end
 end
+
+describe "the update a post process" do
+  it "updates a new post" do
+    category = Category.create(:name => 'Bike')
+    post = Post.create(:title => 'New Bike', :contributor => 'Mary', :description => "Cool Bike", :image => 'image', :category_id => category.id)
+    visit category_path(category)
+    click_on 'Update Post'
+    fill_in 'Title', :with => "Big Bike"
+    fill_in 'Contributor', :with => "Sue Marie"
+    fill_in 'Description', :with => "Nice Bike"
+    fill_in 'Image', :with => "Image"
+    click_on 'Submit'
+    expect(page).to have_content 'Big Bike'
+  end
+end
