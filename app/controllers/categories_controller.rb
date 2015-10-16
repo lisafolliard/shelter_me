09d42d1089a13fd833d06e1c2c57ff24db2f6b46
Lisa Folliard <lisaf37@gmail.com>
2015-10-16 11:33:51 -0700

    delete a category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -33,6 +33,12 @@ class CategoriesController < ApplicationController
     end
   end
 
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+    redirect_to categories_path
+  end
+
   private
   def category_params
     params.require(:category).permit(:name)
