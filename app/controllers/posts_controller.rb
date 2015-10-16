0b36bd468f8cea218198fea72fd4614112022f30
Lisa Folliard <lisaf37@gmail.com>
2015-10-16 16:28:53 -0700

    add flash messages

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,6 +8,7 @@ class PostsController < ApplicationController
     @category = Category.find(params[:category_id])
     @post = @category.posts.new(post_params)
     if @post.save
+      flash[:notice] = "Post successfully added!"
       redirect_to category_path(@post.category)
     else
       render :new
