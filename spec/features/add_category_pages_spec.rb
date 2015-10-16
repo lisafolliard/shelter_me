6c4d77b1fdd2368f1e079ba40f6e915d20baecb1
Lisa Folliard <lisaf37@gmail.com>
2015-10-16 11:30:45 -0700

    pass integration spec to test update a category process

--- spec/features/add_category_pages_spec.rb
+++ spec/features/add_category_pages_spec.rb
@@ -9,3 +9,17 @@ describe "the add a category process" do
     expect(page).to have_content "Bike"
   end
 end
+
+describe "the update a category process" do
+  it "updates a new category" do
+    visit categories_path
+    click_on "Add Category"
+    fill_in 'Name', :with => "Bike"
+    click_on 'Submit'
+    click_on "Bike"
+    click_on "Update Category"
+    fill_in 'Name', :with => 'Bikes'
+    click_on 'Submit'
+    expect(page).to have_content "Bikes"
+  end
+end
