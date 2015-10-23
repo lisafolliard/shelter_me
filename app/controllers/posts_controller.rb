23000881a4d2018b92d2cc0401f21754bb558a37
Lisa Folliard <lisaf37@gmail.com>
2015-10-22 20:30:58 -0700

    add css styling; adjust routing

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -67,7 +67,7 @@ class PostsController < ApplicationController
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
-    redirect_to posts_path
+    redirect_to root_path
   end
 
   private
--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -20,6 +20,25 @@ class TagsController < ApplicationController
     end
   end
 
+  def edit
+    @tag = Tag.find(params[:id])
+  end
+
+  def update
+    @tag = Tag.find(params[:id])
+    if @tag.update(tag_params)
+      redirect_to tag_path(@tag)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @tag = Tag.find(params[:id])
+    @tag.destroy
+    redirect_to root_path
+  end
+
   private
   def tag_params
     params.require(:tag).permit(:name)
