a5b7bfb7b9e3cac4cdfbed032a424e0ce24681b1
Lisa Folliard <lisaf37@gmail.com>
2015-10-22 21:54:06 -0700

    pass spec to edit a post

--- spec/features/add_post_pages_spec.rb
+++ spec/features/add_post_pages_spec.rb
@@ -1,15 +1,5 @@
 require 'rails_helper'
 
-# describe "the add a post process" do
-#   it "adds a new post" do
-#     visit root_path
-#     click_link "Add Post"
-#     post = FactoryGirl.create(:post)
-#     click_on 'Submit'
-#     expect(page).to have_content "Big Bike"
-#   end
-# end
-
 describe "the add a post process" do
   it "add a post" do
     visit root_path
--- spec/features/edit_post_pages_spec.rb
+++ spec/features/edit_post_pages_spec.rb
@@ -1,35 +1,21 @@
-# require 'rails_helper'
-#
-# describe "the update a post process" do
-#   it "updates a post" do
-#     category = Category.create(:name => 'Bike')
-#     post = Post.create(:title => 'New Bike', :contributor => 'Mary', :description => "Cool Bike", :image => 'image', :category_id => category.id)
-#     visit category_path(category)
-#     click_on 'Update Post'
-#     fill_in 'Title', :with => "Big Bike"
-#     fill_in 'Contributor', :with => "Sue Marie"
-#     fill_in 'Description', :with => "Nice Bike"
-#     fill_in 'Image', :with => "Image"
-#     click_on 'Submit'
-#     expect(page).to have_content 'Big Bike'
-#   end
-# end
+require 'rails_helper'
 
-#
-# describe "the edit a post process" do
-#   it "edits a post" do
-#     visit root_path
-#     click_on "Add Post"
-#     post = FactoryGirl.create(:post)
-#     click_on 'Submit'
-#     post.title.should eq "Big Bike"
-#     click_link "Big Bike"
-#     click_on "Edit Post"
-#     fill_in "title", with: "Tandem"
-#     fill_in 'contributor', with: 'suesue'
-#     fill_in 'Description', with: 'cool double bike'
-#     fill_in 'image', with: 'image'
-#     click_on 'Submit'
-#     expect(page).to have_content "Tandem"
-#   end
-# end
+describe "the add a post process" do
+  it "add a post" do
+    visit root_path
+    click_on "Add Post"
+    fill_in 'Title', :with => "Treehouse"
+    fill_in 'Contributor', :with => "Sue Marie"
+    fill_in 'Description', :with => "Nice house"
+    fill_in 'Image', :with => "Image"
+    click_on 'Submit'
+    click_on 'Treehouse'
+    click_on "Edit Post"
+    fill_in 'Title', :with => "Shoebox"
+    fill_in 'Contributor', :with => "Sue Marie"
+    fill_in 'Description', :with => "Nice house"
+    fill_in 'Image', :with => "Image"
+    click_on 'Submit'
+    expect(page).to have_content 'Shoebox'
+  end
+end
