edfe3a667892623e11472b7caa26bda6a425ffe1
Lisa Folliard <lisaf37@gmail.com>
2015-10-22 21:51:58 -0700

    pass spec to add a post

--- spec/features/add_post_pages_spec.rb
+++ spec/features/add_post_pages_spec.rb
@@ -1,5 +1,5 @@
-# require 'rails_helper'
-#
+require 'rails_helper'
+
 # describe "the add a post process" do
 #   it "adds a new post" do
 #     visit root_path
@@ -8,12 +8,17 @@
 #     click_on 'Submit'
 #     expect(page).to have_content "Big Bike"
 #   end
-#
-#   it 'displays errors when the form is incomplete' do
-#     visit root_path
-#     click_link 'Add Post'
-#     click_on 'Submit'
-#     expect(page).to have_content "errors"
-#   end
-#
 # end
+
+describe "the add a post process" do
+  it "add a post" do
+    visit root_path
+    click_on "Add Post"
+    fill_in 'Title', :with => "Treehouse"
+    fill_in 'Contributor', :with => "Sue Marie"
+    fill_in 'Description', :with => "Nice house"
+    fill_in 'Image', :with => "Image"
+    click_on 'Submit'
+    expect(page).to have_content 'Treehouse'
+  end
+end
